bf0114a9456520737c14b1aa55005a7f671ba78e
Ray Ortiz <oray@bgsu.edu>
2022-09-21 07:43:53 -0400

    add fetchNFTsForCollection

--- rtw3-week4/pages/index.js
+++ rtw3-week4/pages/index.js
@@ -7,7 +7,7 @@ const Home = () => {
   const [collection, setCollectionAddress] = useState("");
   const [NFTs, setNFTs] = useState([]);
 
-  const fetchNFTs = async() => {
+  const fetchNFTs = async () => {
     let nfts;
     console.log("fetching nfts");
     const apiKey = "6xkPfdAvIZ-1xZt3YSLG4OS6iEIAU9Ls";
@@ -15,23 +15,36 @@ const Home = () => {
     if (!collection.length) {
       // Setup request options:
       var requestOptions = {
-        method: "GET"
+        method: "GET",
       };
-
 
       const fetchURL = `${baseURL}?owner=${wallet}`;
 
       nfts = await fetch(fetchURL, requestOptions).then((data) => data.json());
     } else {
-      console.log("Fetching nfts for collection owned by address")
+      console.log("Fetching nfts for collection owned by address");
       const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}`;
       nfts = await fetch(fetchURL, requestOptions).then((data) => data.json());
     }
     if (nfts) {
-      console.log("nfts:",nfts);
-      setNFTs(nfts.ownedNfts)
+      console.log("nfts:", nfts);
+      setNFTs(nfts.ownedNfts);
     }
   };
+  const fetchNFTsForCollection = async () => {
+    if (collection.length) {
+      const apiKey = "6xkPfdAvIZ-1xZt3YSLG4OS6iEIAU9Ls";
+      const baseURL = `https://eth-mainnet.alchemyapi.io/nft/v2/${apiKey}/getNFTsForCollection/`;
+      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
+      const nfts = await fetch(fetchURL, requestOptions).then((data) =>
+        data.json()
+      );
+      if (nfts) {
+        console.log("NFTs in collection:", nfts);
+      }
+    }
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <div>
