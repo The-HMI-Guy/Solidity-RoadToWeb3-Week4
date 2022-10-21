0533530825118c7a35daca1d59e6f5f5bd4293fc
Ray Ortiz <oray@bgsu.edu>
2022-10-21 08:32:34 -0400

    nft description and etherscan button added

--- rtw3-week4/components/nftCard.jsx
+++ rtw3-week4/components/nftCard.jsx
@@ -7,7 +7,23 @@ export const NFTCard = ({ nft }) => {
       <div>
         <h2>{nft.title}</h2>
         <p>{nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
-        <p>{`${nft.contract.address.substr(0,6)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
+        <p>{`${nft.contract.address.substr(
+          0,
+          6
+        )}...${nft.contract.address.substr(
+          nft.contract.address.length - 4
+        )}`}</p>
+      </div>
+      <div>
+        <p>{nft.description?.substr(0, 150)}</p>
+      </div>
+      <div>
+        <a
+          target={"_blank"}
+          href={`https://etherscan.io/address/${nft.contract.address}`}
+        >
+          View On Etherscan
+        </a>
       </div>
     </div>
   );
