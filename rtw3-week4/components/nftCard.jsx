fa63d0ad597e0d4b125f4f256ccb36221af5e66f
Ray Ortiz <oray@bgsu.edu>
2022-10-21 08:15:16 -0400

    add shorten contract to NFTCard

--- rtw3-week4/components/nftCard.jsx
+++ rtw3-week4/components/nftCard.jsx
@@ -6,7 +6,8 @@ export const NFTCard = ({ nft }) => {
       </div>
       <div>
         <h2>{nft.title}</h2>
-        <p>{nft.tokenId}</p>
+        <p>{nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
+        <p>{`${nft.contract.address.substr(0,6)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
       </div>
     </div>
   );
