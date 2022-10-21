6cb56331108188a56236044c9ded83815d41bbe6
Ray Ortiz <oray@bgsu.edu>
2022-10-21 07:51:46 -0400

    added image to the NFTCard

--- rtw3-week4/components/nftCard.jsx
+++ rtw3-week4/components/nftCard.jsx
@@ -1,7 +1,13 @@
-export const nftCard = ({nft}) => {
-    return (
-        <div>
-            <p>{nft.title}</p>
-        </div>
-    )
-}
+export const NFTCard = ({ nft }) => {
+  return (
+    <div>
+      <div>
+        <img src={nft.media[0].gateway} alt="nft-image" />
+      </div>
+      <div>
+        <h2>{nft.title}</h2>
+        <p>{nft.tokenId}</p>
+      </div>
+    </div>
+  );
+};
