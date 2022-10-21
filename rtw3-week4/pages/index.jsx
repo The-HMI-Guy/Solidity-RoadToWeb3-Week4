68733e87a26a73bf33aebda4e6e4b4ab1c324394
Ray Ortiz <oray@bgsu.edu>
2022-10-21 08:52:14 -0400

    more styling added to the header controls

--- rtw3-week4/pages/index.jsx
+++ rtw3-week4/pages/index.jsx
@@ -65,6 +65,7 @@ const Home = () => {
           placeholder="Add Your Wallet Address"
         ></input>
         <input
+          className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
           onChange={(e) => {
             setCollectionAddress(e.target.value);
           }}
@@ -72,14 +73,16 @@ const Home = () => {
           type={"text"}
           placeholder="Add The Collection Address"
         ></input>
-        <label>
+        <label className="text-gray-600">
           <input
             onChange={(e) => setFetchForCollection(e.target.checked)}
             type={"checkbox"}
+            className="mr-2"
           ></input>
           Fetch for collection
         </label>
         <button
+          className="disbaled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"
           onClick={() => {
             if (fetchForCollection) {
               fetchNFTsForCollection();
