f48637181382518f659a5503d6c2450a184dc710
Ray Ortiz <oray@bgsu.edu>
2022-10-21 08:45:02 -0400

    disable wallet input box if collection is toggled

--- rtw3-week4/pages/index.jsx
+++ rtw3-week4/pages/index.jsx
@@ -55,6 +55,7 @@ const Home = () => {
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
         <input
+          disabled={fetchForCollection}
           className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
           onChange={(e) => {
             setWalletAddress(e.target.value);
